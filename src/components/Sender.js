6d84d43ad387e86e27fbf16f3dbe7b969adee924
narsenico <not_amon-dev@yahoo.it>
2020-07-06 00:40:48 +0200

    test

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -2,6 +2,21 @@ import React from 'react';
 import axios from 'axios';
 
 function Sender({ calendar, className }) {
+    const test = async () => {
+        try {
+            const res = await axios.get(
+                'http://localhost:3001/hello'
+            );
+            console.log(res);
+            alert('test ok');
+        } catch (e) {
+            console.error(e);
+            alert(
+                `Si è verificato un errore durante l'invio dei dati.\nControllare i log del browser per maggiori informazioni.`
+            );
+        }
+    }
+
     const onClick = async () => {
         try {
             const res = await axios.put(
@@ -19,6 +34,9 @@ function Sender({ calendar, className }) {
 
     return (
         <div className={className}>
+            <button onClick={test}>
+                test
+            </button>
             <button onClick={onClick}>
                 <span className="material-icons">send</span>
             </button>
